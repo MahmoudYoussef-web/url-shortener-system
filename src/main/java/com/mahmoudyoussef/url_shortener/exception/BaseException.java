76c768eb35dccd771ddb665bec1393a3dd28f9b7
MahmoudYoussef-web <lime24864@gmail.com>
2026-04-03 22:29:54 +0200

    feat: add global exception handling

---
+++ src/main/java/com/mahmoudyoussef/url_shortener/exception/BaseException.java
@@ -0,0 +1,15 @@
+package com.mahmoudyoussef.url_shortener.exception;
+
+public abstract class BaseException extends RuntimeException {
+
+    private final int status;
+
+    public BaseException(String message, int status) {
+        super(message);
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
