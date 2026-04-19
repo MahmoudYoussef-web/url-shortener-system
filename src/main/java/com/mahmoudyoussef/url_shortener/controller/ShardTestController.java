f5a57a574c628626ec1ac80c8ba374ef84bda590
MahmoudYoussef-web <lime24864@gmail.com>
2026-04-19 21:28:56 +0200

    docs: update sharding behavior and cache flow to reflect multi-shard deployment and TTL alignment

--- src/main/java/com/mahmoudyoussef/url_shortener/controller/ShardTestController.java
+++ src/main/java/com/mahmoudyoussef/url_shortener/controller/ShardTestController.java
@@ -1,5 +1,6 @@
 package com.mahmoudyoussef.url_shortener.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Profile("dev")
 public class ShardTestController {
 
     private final ShardedUrlRepository repo;
